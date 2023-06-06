c19d633be59c2b095093d9c74e5ef195cab5370e
Andrew Tee <atee001@ucr.edu>
2023-06-06 16:35:19 -0700

    Implementing Print with CUDA

--- src/kernel.cu
+++ src/kernel.cu
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <opencv2/opencv.hpp>
 
 #define TILE_SIZE 16
@@ -25,6 +26,10 @@ __global__ void matAdd(int dim, const float *A, const float *B, float* C) {
 void basicMatAdd(int dim, const float *A, const float *B, float *C)
 {
     // Initialize thread block and kernel grid dimensions ---------------------
+
+    dim3 threadsPerBlock(TILE_SIZE, TILE_SIZE, 1);
+    dim3 blocksPerDim(ceil(dim/(float)threadsPerBlock.x), ceil(dim/(float)threadsPerBlock.y), 1);
+    matAdd<<<blocksPerDim, TILE_SIZE>>>(dim, A, B, C);
 
 
 }
